33d53d58ba5a7b8f1282aa4c2129d425c1e6ea95
shreyansh-jain <shreyanshbafna.310@gmail.com>
2020-12-17 00:56:52 +0530

    Basic version complete

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:plant_app/constants.dart';
-import 'package:plant_app/screens/details/components/icon_cart.dart';
 import 'package:plant_app/screens/details/components/images_and_icons_card.dart';
+import 'package:plant_app/screens/details/components/title_and_card.dart';
+import 'package:plant_app/constants.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: ImagesAndIconsCard(size: size),
+      child: Column(
+        children: <Widget>[
+          ImagesAndIconsCard(size: size),
+          TitleAndPrice(title: "Angelina", price: 440, country: "India"),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                  width: size.width / 2,
+                  height: 84,
+                  child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.only(topRight: Radius.circular(20))),
+                      color: kPrimaryColor,
+                      onPressed: () {},
+                      child: Text(
+                        "Buy Now",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ))),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text("Description"),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/details/components/title_and_card.dart
@@ -0,0 +1,42 @@
+import 'package:plant_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class TitleAndPrice extends StatelessWidget {
+  const TitleAndPrice({
+    Key key,
+    this.title,
+    this.country,
+    this.price,
+  }) : super(key: key);
+
+  final String title, country;
+  final int price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Row(
+          children: <Widget>[
+            RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: "$title\n",
+                  style: Theme.of(context).textTheme.headline4.copyWith(
+                      color: kTextColor, fontWeight: FontWeight.bold)),
+              TextSpan(
+                  text: "$country",
+                  style: TextStyle(
+                      color: kPrimaryColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300))
+            ])),
+            Spacer(),
+            Text("\₹$price",
+                style: Theme.of(context).textTheme.headline5.copyWith(
+                      color: kPrimaryColor,
+                    ))
+          ],
+        ));
+  }
+}
